e441f009cd7b5954911b89e4f76479d0d146c08f
Green80X <green80xx@gmail.com>
2021-10-30 18:01:03 +0800

    updates to keith.c

--- Skeleton_Party/keith.c
+++ Skeleton_Party/keith.c
@@ -21,7 +21,7 @@
 #define COLOR_BLUE CP_Color_Create(0, 0, 255, 255)
 #define COLOR_WHITE CP_Color_Create(255, 255, 255, 255)
 #define COLOR_RED CP_Color_Create(255, 0, 0, 255)
-#define COLOR_BLACK CP_Color_Create(255, 255, 255, 255)
+#define COLOR_BLACK CP_Color_Create(0, 0, 0, 255)
 
 CP_Image gameBackground = NULL;
 
