15c529573dc961ad51199c87e385e091e9eb9b05
qiuhui <85561775+cqblop@users.noreply.github.com>
2021-11-12 16:19:07 +0800

    storyline text (begin and end)
    not yet place in game.c

--- Skeleton_Party/qh.c
+++ Skeleton_Party/qh.c
@@ -16,6 +16,7 @@ struct character
 
 
 
+
 void init_char(struct character *spritename, float spawnx, float spawny, char* pathname)
 {
     spritename->position = CP_Vector_Set(spawnx, spawny);
@@ -24,6 +25,38 @@ void init_char(struct character *spritename, float spawnx, float spawny, char* p
     spritename->height = 200;
     spritename->speed = 100;
 
+}
+
+/*void enemypattern1(void)
+{
+    struct character enemy;
+    
+    float x = CP_System_GetWindowWidth() / 3;
+    float y = CP_System_GetWindowHeight() / 3;
+    for (int i = 0; i < 3; i++)
+    {
+        init_char(enemy, (x+(x*i)), (y+(y*i)), "./Assets/Enemydot.png");
+        
+    }
+}
+*/
+
+void begintext(void)
+{
+    float x = (float)CP_System_GetWindowWidth() / 2;
+    float y = (float)CP_System_GetWindowHeight() / 2;
+    //color fill
+    CP_Graphics_DrawRect((x / 2), (y / 2), x, y);
+    CP_Font_DrawText("Welcome to the skeleton party, please save our princess!", ((x / 2) - 50), ((y / 2) - 50));
+}
+
+void endtext(void)
+{
+    float x = (float)CP_System_GetWindowWidth() / 2;
+    float y = (float)CP_System_GetWindowHeight() / 2;
+    //color fill
+    CP_Graphics_DrawRect((x / 2), (y / 2), x, y);
+    CP_Font_DrawText("Thank you for saving our princess! ", ((x / 2) - 50), ((y / 2) - 50));
 }
 
 void game_control(struct character *spritename )
